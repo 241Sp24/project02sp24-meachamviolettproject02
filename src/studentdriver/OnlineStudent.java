356300aa7267cf6b3c984968b85f38282bdbe42e
Cael Meacham <158321267+Cmeacham27@users.noreply.github.com>
2024-03-29 11:40:10 -0500

    OnlineStudent

---
+++ src/studentdriver/OnlineStudent.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentdriver;
+
+/**
+ *
+ * @author s560346
+ */
+public class OnlineStudent extends StudentFees{
+    private int noOfMonths;
+    private double MONTHLY_FEE = 1245.25;
+
+    public OnlineStudent(String studentName, int studentId, boolean isEnrolled, int noOfMonths) {
+        super(studentName, studentId, isEnrolled);
+    }
+    
+    public String toString(){
+        return"";
+    }
+}
