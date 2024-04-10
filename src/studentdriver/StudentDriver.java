f49ec6f1b5dbc5637ab9f3d8ffa3a09a992a9282
dayvinV <s554635@nwmissouri.edu>
2024-04-10 11:30:32 -0500

    print stuff

--- src/studentdriver/StudentDriver.java
+++ src/studentdriver/StudentDriver.java
@@ -17,22 +17,35 @@ public class StudentDriver {
     public static void main(String[] args) throws FileNotFoundException {
         //create the array of students
         StudentFees[] students = new StudentFees[12];
+        File inputFile = new File("input.csv");
 
         //get number of students from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the no of UG Students: ");
-         int ugStudent = input.nextInt();
+        int ugStudent = input.nextInt();
         System.out.print("Enter the no of Graduate Students: ");
         int gradStudent = input.nextInt();
         System.out.print("Enter the no of Online Students: ");
         int onlineStudent = input.nextInt();
-        
+
         System.out.println("\n**********Undergraduate students list**********");
         for (StudentFees s : students) {
-           if(s instanceof UGStudent){
-               System.out.println(s);
-           }
+            if (s instanceof UGStudent) {
+                System.out.println(s);
+            }
+        }
+        System.out.println("\n**********Graduate students list**********");
+        for (StudentFees s : students) {
+            if (s instanceof GraduateStudent) {
+                System.out.println(s);
+            }
+        }
+        System.out.println("\n**********Online students list**********");
+        for (StudentFees s : students) {
+            if (s instanceof OnlineStudent) {
+                System.out.println(s);
+            }
         }
     }
-    
+
 }
